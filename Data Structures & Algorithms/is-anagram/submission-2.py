e70b730a766a68752bef284cf3427dd35e41352b
Vivek Mishra <32127549+vivek721@users.noreply.github.com>
2026-07-15 21:43:45 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) < len(t):
+            s,t=t,s
+        print(t,s)
+        for str in t:
+            s = s.replace(str,"",1)
+
+        return len(s) == 0
